19371b4ec5bd0b9df6eac2698aff2a79c0f97cc6
egor.khober <egor.khober@arcadia.spb.ru>
2017-03-02 23:17:21 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './app/index.html'})
+  ]
+};
