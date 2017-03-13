472aba72012e0135e2e42eeda4f2f0c5a66d7fd2
Egor Khober <altagoga@gmail.com>
2017-03-13 03:37:04 +0300

    added infowindow with api calls

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import css from '../css/main.css';
-var ko = require("knockout");
+import mobilecss from '../css/mobile.css';
 import { concerts } from './model.js';
 
+var ko = require("knockout");
 const SAVE_SEARCH_STR = 'SAVE_SEARCH_STR';
+var lastFmApiKey = '9ff1cbd1b11a8d927f184d1ff1fe2c36';
+var lastFmApiPath = 'http://ws.audioscrobbler.com/2.0/';
 var panelToggler = {};
+var GoogleMapsLoader = require('google-maps');
+GoogleMapsLoader.KEY = 'AIzaSyDm9nLO6iek_naPFgnyMz1JsJV-TEhLU5k';
+var map;
+var markers = [];
+var largeInfoWindow;
 var currentConcerts;
 var searchStr = getSearchStr();
 if (searchStr) {
@@ -18,8 +26,6 @@ if (searchStr) {
 } else {
     currentConcerts = concerts;
 }
-
-
 
 
 var ViewModel = function() {
@@ -45,11 +51,13 @@ var ViewModel = function() {
             return concert.band.includes(self.searchStr()) || concert.place.includes(self.searchStr());
         }));
         replaceMarkers(self.concerts());
+        closeInfoWindow(largeInfoWindow);
         saveSearchStr(self.searchStr());
     }
 
     self.centerToConcert = function(concert) {
         centerToMarker(concert);
+        getBandInfo(concert);
     };
 };
 ko.applyBindings(new ViewModel());
@@ -67,11 +75,8 @@ function getSearchStr() {
 }
 
 
-var GoogleMapsLoader = require('google-maps');
-var map;
-var markers = [];
 
-GoogleMapsLoader.KEY = 'AIzaSyDm9nLO6iek_naPFgnyMz1JsJV-TEhLU5k';
+
 
 GoogleMapsLoader.load(function(google) {
     var marker;
@@ -83,6 +88,7 @@ GoogleMapsLoader.load(function(google) {
     currentConcerts.forEach(function(concert) {
         markers.push(createMarker(concert));
     });
+    largeInfoWindow = new google.maps.InfoWindow();
 });
 
 
@@ -90,12 +96,35 @@ function createMarker(concert) {
     var marker = new google.maps.Marker({
         position: { lat: concert.lat, lng: concert.lng },
         map: map,
-        title: concert.band + '/' + concert.date
+        title: concert.band + '/' + concert.date,
+        icon: getMarkerIcon(concert.date),
+        animation: google.maps.Animation.DROP
     });
     marker.concertId = concert.id;
+    marker.addListener('click', function() {
+        getBandInfo(concert);
+    });
+
     return marker;
 }
 
+
+function getMarkerIcon(concertDate) {
+    var currentDate = +new Date();
+    concertDate = convertDateToTimestamp(concertDate);
+    var timeToConcert = concertDate - currentDate;
+    if (timeToConcert < 1000 * 60 * 60 * 24 * 7) {
+        return 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+    } else if (timeToConcert < 1000 * 60 * 60 * 24 * 30) {
+        return 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+    } else {
+        return 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+    }
+}
+
+function convertDateToTimestamp(concertDate) {
+    return new Date(concertDate.split('.').reverse().join('-')).getTime();
+}
 
 function replaceMarkers(concerts) {
     var concertsMap = new Map();
@@ -108,15 +137,116 @@ function replaceMarkers(concerts) {
         if (!concertsMap.has(marker.concertId)) {
             marker.setMap(null);
         } else {
+            marker.setMap(map);
             concertsMap.delete(marker.concertId);
         }
     });
-
     concertsMap.forEach(function(concert) {
         markers.push(createMarker(concert));
     });
+}
 
 
+function populateInfoWindow(concert, infoWindow) {
+    var marker = markers.find(function(marker) {
+        return marker.concertId === concert.id;
+    });
+    if (infoWindow.marker != marker) {
+        if (infoWindow.marker) {
+            infoWindow.marker.setAnimation(null);
+        }
+        infoWindow.marker = marker;
+        infoWindow.setContent(createInfoWindowContent(concert));
+        infoWindow.open(map, marker);
+        infoWindow.addListener('closeclick', function() {
+            infoWindow.marker.setAnimation(null);
+            infoWindow.marker = null;
+        });
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        var streetViewService = new google.maps.StreetViewService();
+        var radius = 50;
+        function getStreetView(data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+                var nearStreetViewLocation = data.location.latLng;
+                var heading = google.maps.geometry.spherical.computeHeading(
+                    nearStreetViewLocation, marker.position);
+                var panoramaOptions = {
+                    position: nearStreetViewLocation,
+                    pov: {
+                        heading: heading,
+                        pitch: 30
+                    }
+                };
+                var panorama = new google.maps.StreetViewPanorama(
+                    document.getElementById('pano'), panoramaOptions);
+            } else {
+                document.getElementById('pano').innerHTML = "No Street View Found";
+            }
+        }
+        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+    } else {
+        closeInfoWindow(infoWindow);
+    }
+}
+
+
+
+
+
+
+function closeInfoWindow(infoWindow) {
+    infoWindow.marker.setAnimation(null);
+    infoWindow.marker = null;
+    infoWindow.close();
+}
+
+
+function createInfoWindowContent(concert) {
+    var template = `<div id="infoWindow"><h4>Band: %bandName%</h4>
+					<div>%img%</div>
+					<h4 id="placeInfoHeader">Place: %place% / Date: %date%</h4>
+					<div id="pano"></div>
+					</div>`;
+    var imgTemplate = '<img src=%bandImg%></img>';
+
+
+    var res = template.replace('%bandName%', concert.band)
+        .replace('%place%', concert.place)
+        .replace('%date%', concert.date);
+    if (concert.bandInfo && concert.bandInfo.image[2]['#text']) {
+        res = res.replace('%img%', imgTemplate.replace('%bandImg%', concert.bandInfo.image[2]['#text']));
+    } else {
+        res = res.replace('%img%', "Can't get image of band");
+    }
+
+    return res;
+}
+
+
+function getBandInfo(concert) {
+    if (!concert.bandInfo) {
+        $.ajax({
+            url: lastFmApiPath,
+            data: {
+                'api_key': lastFmApiKey,
+                'method': 'artist.search',
+                'artist': concert.band,
+                'format': 'json'
+            },
+            success: function(data) {
+                if (+data.results['opensearch:totalResults'] > 0) {
+                    concert.bandInfo = data.results.artistmatches.artist[0];
+                }
+                populateInfoWindow(concert, largeInfoWindow);
+            },
+            error: function() {
+                console.error("Can't get info about band");
+                populateInfoWindow(concert, largeInfoWindow);
+            }
+        });
+    } else {
+        populateInfoWindow(concert, largeInfoWindow);
+    }
 }
 
 function centerToMarker(concert) {
